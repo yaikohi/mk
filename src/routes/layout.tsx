699afa376c9c2e1bb9d0150c22da18793895bbba
ykhi <erik.beem@hotmail.com>
2023-06-01 17:26:17 +0200

    chore: add tailwind, remove baseplate

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,26 +1,11 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-import styles from './styles.css?inline';
-
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
+import { component$, Slot } from '@builder.io/qwik';
 
 export default component$(() => {
-  useStyles$(styles);
   return (
     <>
-      <Header />
       <main>
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
