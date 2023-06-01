94a9dc45395adf188fca0066694584b93187a753
ykhi <erik.beem@hotmail.com>
2023-06-02 00:52:34 +0200

    style: add shadcn/button component

--- src/routes/sign-in/index.tsx
+++ src/routes/sign-in/index.tsx
@@ -1,12 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 import { Form } from '@builder.io/qwik-city';
+import Button from '~/components/router-head/button';
 import { useAuthSignin } from '~/routes/plugin@auth';
 export default component$(() => {
     const signIn = useAuthSignin();
     return (
-        <Form class="flex bg-muted p-3 rounded-xl" action={signIn}>
+        <Form class="flex p-3 rounded-xl place-items-center justify-middle" action={signIn}>
             <input type="hidden" name="providerId" value="github" />
-            <button class="px-3 py-2 rounded-xl bg-slate-100 hover:slate-200">Sign In</button>
+            <Button variant={"default"}>Sign In</Button>
         </Form>
     )
 });
