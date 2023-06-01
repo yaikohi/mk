bb957b956935cc4d4c0004dbee337ab351111ea9
ykhi <erik.beem@hotmail.com>
2023-06-02 00:04:13 +0200

    style: make navbar pretty!!!!!

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,5 +1,6 @@
 import { component$ } from '@builder.io/qwik';
-
+import { LuGithub, LuMoon, LuTwitter } from "@qwikest/icons/lucide";
+import { LuSun } from "@qwikest/icons/lucide";
 export const Navbar = component$(() => {
     const routes = [
         {
@@ -15,16 +16,26 @@ export const Navbar = component$(() => {
             path: "/sign-in"
         }
     ]
-    return (<nav>
-        <ul>
-            {routes.map((route, id) => {
-                return (<li key={id}>
-                    <a href={route.path}>
-                        {route.name}
-                    </a>
-                </li>)
-            })}
-            <li></li>
-        </ul>
-    </nav>)
+    return (
+        <nav class="container flex h-14 items-center justify-between border-b-2">
+            <div class="flex gap-8 items-center">
+                <h1>\mk</h1>
+                <ul class="flex text-sm gap-4 font-light">
+                    {routes.map((route, id) => {
+                        return (
+                            <li key={id} class="">
+                                <a href={route.path}>
+                                    {route.name}
+                                </a>
+                            </li>)
+                    })}
+                </ul>
+            </div>
+            <div class="flex gap-8 items-center">
+                <LuGithub />
+                <LuTwitter />
+                <LuMoon />
+                <LuSun />
+            </div>
+        </nav>)
 });
